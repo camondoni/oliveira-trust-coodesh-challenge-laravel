39a93d694a5d091c285b46fcbc1e259e90316e9f
Caio <caio.mondoni@mypocketpay.com>
2022-06-16 21:48:58 -0300

    adicionado .env ao repositorio

--- startScript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-composer install
-#php artisan migrate:fresh
-#php artisan db:seed --class=CotationFeeSeeder
-nohup php artisan serve --host=0.0.0.0 &
-cd /var/www/react
-npm install
-nohup npm start &
